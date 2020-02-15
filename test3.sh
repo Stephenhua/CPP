3c07bc24f387ff6c98e523ddf8f97b780db958a3
Setphenhua <04121390@cumt.edu.cn>
2020-02-15 22:58:22 +0800

    “2020-2-15 shell 学习”

---
+++ test3.sh
@@ -0,0 +1,29 @@
+
+#!/bin/bash
+# valodAlphaNum-- 确保输入内容仅限于字母和数字
+
+validAphaNum(){
+    #返回值：如果数入得内容全部是字母和数字，返回0，负责返回1；
+    # 删除所有不符合要求的字符
+
+    validChars ="$(echo $1 | sed -e 's/[^[:alnum:]]//g')"
+
+    if ["$validChars"="$1"]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+#==============================================
+#主脚本
+/bin/bash -n "Enter input:"
+read input
+#输入验证
+if ! validAphaNum "$input";then
+    echo "Your input must consist of only letters and numbers.">&2
+    exit 1
+else
+    echo "Input is valid "
+fi
+exit 0
