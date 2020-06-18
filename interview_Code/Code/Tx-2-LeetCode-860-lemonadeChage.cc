854bab98329be2e97c2daa92af0b8f78d6b8cac1
Stephenhua <04121390@cumt.edu.cn>
2020-06-18 23:52:45 +0800

    lemmong

--- interview_Code/Code/Tx-2-LeetCode-860-lemonadeChage.cc
+++ interview_Code/Code/Tx-2-LeetCode-860-lemonadeChage.cc
@@ -55,9 +55,68 @@ using namespace std;
 bills[i] 不是 5 就是 10 或是 20 
 */
 
+//方法一：直接使用hash表进行实现；
+
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
+        unordered_map<int,int> mp ;
+        for(int i :bills){
+            if( i == 5){
+                mp[5]++;
+            }else if( i == 10){
+                mp[10]++;
+                mp[5]--;
+            }else if( i == 20 && mp[10] > 0){
+                mp[10]--;
+                mp[5]--;
+            }else{
+
+                mp[5] -=3;
+            }
+
+            if( mp[5] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+//方法二： 直接在每次进行判断；对于每种情况都进行了判断；
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0 ,ten = 0;
+        for(int i = 0 ;i < bills.size() ;i ++){
+
+            if( bills[i] == 5){
+                five++;
+            }else if( bills[i] == 10){
+                if( five == 0 ){
+                    return false;
+                }
+                ten++;
+                five--;
+            }else if( bills[i] == 20){
+                if( five && ten){
+                    five--;
+                    ten--;
+                    if( five < 0 || ten < 0){
+                        return false;
+                    }
+                }else{
+                    five -= 3;
+                    if( five < 0 ){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
 
     }
+
 };
